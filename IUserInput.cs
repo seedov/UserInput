565b83ef0db5363ee7f431141be0ef3470b23e69
anton_sedov <a.sedov@spherelab.ru>
2019-03-29 14:12:15 +0300

    Interface and Canvas User Input added

---
+++ IUserInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface IUserInput 
+{
+    event Action<Vector2> PointerDownInputReceived;
+    event Action<Vector2> PointerUpInputReceived;
+    event Action<Vector2> InputMoveReceived;
+    event Action<float> InputScaleReceived;
+
+}
---
+++ ScreenUserInput/UiCanvasUserInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Reads user input from Ui image.
+/// </summary>
+[RequireComponent(typeof(Image))]
+public class UiCanvasUserInput : MonoBehaviour, IUserInput, IPointerDownHandler, IPointerUpHandler, IDragHandler, IScrollHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PointerUpInputReceived?.Invoke(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        InputMoveReceived?.Invoke(eventData.delta);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PointerDownInputReceived?.Invoke(eventData.position);
+    }
+    public void OnScroll(PointerEventData eventData)
+    {
+        print(eventData.scrollDelta);
+        InputScaleReceived?.Invoke(eventData.scrollDelta.y);
+    }
+
+    public event Action<Vector2> PointerDownInputReceived;
+    public event Action<Vector2> PointerUpInputReceived;
+    public event Action<Vector2> InputMoveReceived;
+    public event Action<float> InputScaleReceived;
+}
